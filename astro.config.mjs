cc7289e7cc98a5227eb02569e3ee297049615f0d
John Saylor <johnmsaylor@gmail.com>
2026-05-25 11:35:32 -0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [sitemap()],
   site: "https://watchhillguide.com",
   vite: {
     plugins: [tailwindcss()]

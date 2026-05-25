cafbc4decf45ab46b861a9efde0b96edd9e0645e
John Saylor <johnmsaylor@gmail.com>
2026-05-24 23:28:35 -0400

    move CNAME publice and add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://watchhillguide.com",
   vite: {
     plugins: [tailwindcss()]
   }

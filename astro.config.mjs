72cd1e7e2fccdc5142766c29f846489d1a6c3abf
John Saylor <johnmsaylor@gmail.com>
2026-05-25 11:40:39 -0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 
+sitemap({
+  filenameBase: 'sitemap'   // This makes it generate sitemap.xml + sitemap-index.xml
+})
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
